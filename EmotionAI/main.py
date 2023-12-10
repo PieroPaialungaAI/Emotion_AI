530b2d5680591849d60a67db014f47364c648abd
Piero Paialunga <55837093+PieroPaialungaAI@users.noreply.github.com>
2023-12-10 17:52:04 -0500

    Update main.py with comments

--- EmotionAI/main.py
+++ EmotionAI/main.py
@@ -10,11 +10,11 @@ from .constants import *
 from .emotionai import * 
 
 if __name__=='__main__':
-    round_of_emotions = 3
-    for number_of_simulation in range(round_of_emotions):
-        for picked_emotion in EMOTIONS:
-            print('Processing Emotion AI for emotion %s' %(picked_emotion))
-            emotion_generator = AIEmotionGenerator(emotion=picked_emotion,openaikey = OPEN_AI_API_KEY)
-            environment = emotion_generator.build_environment()
-            text_for_emotion = emotion_generator.sentence_generator(environment=environment)
-            print(text_for_emotion)
+    round_of_emotions = 3 #This multiplies the number of times you want to generate emotions (e.g. 3 -> 50x3 = 150 sentences)
+    for _ in range(round_of_emotions):
+        for picked_emotion in EMOTIONS: #Generate sentences for every emotion
+            print('Processing Emotion AI for emotion %s' %(picked_emotion)) #Just a print statement
+            emotion_generator = AIEmotionGenerator(emotion=picked_emotion,openaikey = OPEN_AI_API_KEY) #Calling the class of Emotion Generator 
+            environment = emotion_generator.build_environment() #Build your folder to store the files
+            text_for_emotion = emotion_generator.sentence_generator(environment=environment) #Generate sentences in those environment
+            #print(text_for_emotion) #You can print the text you're saving in the files if you want...
